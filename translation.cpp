04fa5693eac90d380bd2bbb40b77bc90b2f2003a
zhubaishu <cbl_tae@163.com>
2016-03-14 10:41:28 +0800

    Create translation.cpp

---
+++ translation.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    unsigned int va, pa;
+    freopen("input.txt","r",stdin);
+    cin >> va >> pa;
+    unsigned int pde_idx = (va >> 22) & 0x3FF;
+    unsigned int pde_ctx = ((pde_idx - 0x300 + 1) << 12)+ 0x3;
+    unsigned int pte_idx = (va >> 12) & 0x3FF;
+    unsigned int pte_ctx = pa & ~0x3FF | 0x3;
+    printf("va 0x%08x, pa 0x%08x, pde_idx 0x%08x, pde_ctx 0x%08x, pte_idx 0x%08x, pte_ctx 0x%08x\n", 
+    va, pa, pde_idx, pde_ctx, pte_idx, pte_ctx);
+    stdin.close();
+    return 0;
+}
